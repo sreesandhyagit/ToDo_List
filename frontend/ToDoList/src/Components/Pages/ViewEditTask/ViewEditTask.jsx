a7f76b30ec00dc321bd17a61f4bd5dffd5f2075a
sreesandhyagit <sreesandhyaajesh@gmail.com>
2026-05-29 11:14:23 +0530

    Frontend production setup

--- frontend/ToDoList/src/Components/Pages/ViewEditTask/ViewEditTask.jsx
+++ frontend/ToDoList/src/Components/Pages/ViewEditTask/ViewEditTask.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import axios from "axios"
 // import "./ViewEditTask.css"
 
-const API_URL="http://127.0.0.1:8000/"
+const API_URL=`${import.meta.env.VITE_API_URL}/`
 
 function ViewEditTask() {
 
