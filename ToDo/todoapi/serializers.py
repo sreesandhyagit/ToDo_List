6b710cd488f1b7d2738871b124506b35e953664d
sreesandhyagit <sreesandhyaajesh@gmail.com>
2026-05-29 11:25:32 +0530

    Duplicate ToDo Folder deleted

--- ToDo/todoapi/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from .models import Tasks
-class ToDoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Tasks
-        fields=["id","task","is_completed","date"]
--- ToDo/todoapi/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from todoapi import views
-
-urlpatterns = [
-    path("",views.AddViewTask),
-    path("edit-task/<int:task_id>",views.editTask)
-]
